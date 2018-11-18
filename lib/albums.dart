bacc138408a1c785d8ba9b717c7d1b5635530cfe
Uday Tori <kumaruday691@gmail.com>
2018-11-18 12:39:47 -0600

    style changes

--- lib/albums.dart
+++ lib/albums.dart
@@ -8,7 +8,8 @@ class Albums extends StatelessWidget {
   Albums(this.albums) {}
 
   // Properties
-  final List<Album>  albums; // value of the property will not change after setting
+  final List<Album>
+      albums; // value of the property will not change after setting
 
   // Helper Methods
   Widget _buildAlbumItem(BuildContext context, int index) {
@@ -16,14 +17,58 @@ class Albums extends StatelessWidget {
       child: Column(
         children: <Widget>[
           Image.asset(albums[index].imageUrl),
-          Text(albums[index].title),
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                albums[index].title,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                width: 50,
+              ),
+              Container(
+                margin: EdgeInsets.all(10),
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  decoration: BoxDecoration(
+                      color: Theme.of(context).accentColor,
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(Icons.monetization_on, color: Colors.indigo,),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        albums[index].price.toString(),
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.purple),
+                      ),
+                    ],
+                  )),
+            ],
+          ),
+          DecoratedBox(
+            decoration: BoxDecoration(
+                border: Border.all(color: Colors.blueGrey, width: 1.0),
+                borderRadius: BorderRadius.circular(10)),
+            child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 12, vertical: 5),
+                child: Text(albums[index].description)),
+          ),
           ButtonBar(
             alignment: MainAxisAlignment.center,
             children: <Widget>[
               FlatButton(
-                child: Text('Details'),
-                onPressed: () => Navigator.pushNamed<bool>(context, '/album/' + index.toString())
-              )
+                  child: Text('Details'),
+                  onPressed: () => Navigator.pushNamed<bool>(
+                      context, '/album/' + index.toString()))
             ],
           )
         ],
--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -2,13 +2,12 @@ import 'package:flutter/material.dart';
 
 class AuthPage extends StatefulWidget {
   @override
-    State<StatefulWidget> createState() {
-      return _AuthPageState();
-    }
+  State<StatefulWidget> createState() {
+    return _AuthPageState();
+  }
 }
 
-class _AuthPageState extends State<AuthPage>{
-
+class _AuthPageState extends State<AuthPage> {
   String _email;
   String _password;
   bool _saveLogin = false;
@@ -16,47 +15,53 @@ class _AuthPageState extends State<AuthPage>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Login")),
-      body:Container(
-      margin: EdgeInsets.all(10.0),
-      child: ListView(
-      children: <Widget>[
-       TextField(
-          decoration: InputDecoration(labelText: "e-mail Id", prefixIcon: Icon(Icons.person)),
-          keyboardType: TextInputType.emailAddress,
-          onChanged: (String value) {
-            setState(() {
-              _email = value;
-            });
-          },
-        ),
-        TextField(
-          decoration: InputDecoration(labelText: "Password", prefixIcon: Icon(Icons.security)),
-          obscureText: true,
-          onChanged: (String value) {
-            setState(() {
-              _password = value;
-            });
-          },
-        ), 
-        SwitchListTile(value: _saveLogin, onChanged: (bool value) {
-            setState(() {
-                          _saveLogin = !_saveLogin;
-                        });
-        },
-        title: Text("Save Login for next time"),),
-        SizedBox(height: 10.0,),
-        RaisedButton(
-          color: Theme.of(context).accentColor,
-          textColor: Colors.purple,
-          child: Text("Login"),
-          onPressed: () {
-            Navigator.pushReplacementNamed(context, '/albums');
-          },
-
-        )
-      ],
-    ),)
-    );
+        appBar: AppBar(title: Text("Login")),
+        body: Container(
+          margin: EdgeInsets.all(10.0),
+          child: ListView(
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(
+                    labelText: "e-mail Id", prefixIcon: Icon(Icons.person)),
+                keyboardType: TextInputType.emailAddress,
+                onChanged: (String value) {
+                  setState(() {
+                    _email = value;
+                  });
+                },
+              ),
+              TextField(
+                decoration: InputDecoration(
+                    labelText: "Password", prefixIcon: Icon(Icons.security)),
+                obscureText: true,
+                onChanged: (String value) {
+                  setState(() {
+                    _password = value;
+                  });
+                },
+              ),
+              SwitchListTile(
+                value: _saveLogin,
+                onChanged: (bool value) {
+                  setState(() {
+                    _saveLogin = !_saveLogin;
+                  });
+                },
+                title: Text("Save Login for next time"),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              RaisedButton(
+                color: Theme.of(context).accentColor,
+                textColor: Colors.purple,
+                child: Text("Login"),
+                onPressed: () {
+                  Navigator.pushReplacementNamed(context, '/albums');
+                },
+              )
+            ],
+          ),
+        ));
   }
 }
