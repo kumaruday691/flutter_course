8a098167ac952d59b3936ce053530f51dd0f37a7
Uday Tori <kumaruday691@gmail.com>
2018-11-25 10:43:26 -0600

    switch from dark to light mode

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-          brightness: Brightness.dark,
+          brightness: Brightness.light,
           primarySwatch: Colors.indigo,
           accentColor: Colors.tealAccent),
       //home: AuthPage(),
